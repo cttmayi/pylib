876dff096588fe324b7688eb913ae2ee7570c01b
ling.yuan <cttmayi@hotmail.com>
2023-04-24 00:10:45 +0800

    print color

---
+++ pytest/debug/print.py
@@ -0,0 +1,12 @@
+from colorama import Fore
+
+print("NORMAL")
+print(Fore.GREEN + "GREEN")
+print(Fore.BLUE + "BLUE")
+
+print(Fore.YELLOW + "YELLOW")
+print(Fore.CYAN + "CYAN")
+print(Fore.RED + "RED")
+print(Fore.RESET, end='')
+print("RESET")
+
