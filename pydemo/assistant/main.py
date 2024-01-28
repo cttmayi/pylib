251169add610e41bbe56b8785311b2839d2e28f3
ling.yuan <cttmayi@hotmail.com>
2024-01-28 23:09:27 +0800

    update

--- pydemo/assistant/main.py
+++ pydemo/assistant/main.py
@@ -4,8 +4,20 @@ from utils.spinner import Spinner
 def main():
 
     with Spinner('Thinking ...'):
-        import time
-        time.sleep(10)
+        from langchain_openai import ChatOpenAI
+        from prompts.classifier import Classifier
+        from langchain_community.vectorstores import Chroma
+        from langchain_openai import OpenAIEmbeddings
+        llm = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.0)
+        embeddings = OpenAIEmbeddings()
+        db = Chroma('test', embeddings, persist_directory='default.db')
+        classifier = Classifier(llm, db)
+        classifier.init_for_test()
+        question = 'Create a presentation about climate change.'
+        ret = classifier(question)
+        print(ret.content)
+        #assert(ret.content.find('RequiresContext: true') >= 0)
+        #assert(ret.content.find('Categories: presentation') >= 0)
     print('Think Done')
 
 
