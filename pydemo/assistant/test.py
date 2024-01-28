75d510d95b57736f7f1c5f54158f8c11611d6af5
ling.yuan <cttmayi@hotmail.com>
2024-01-28 21:28:48 +0800

    update

--- pydemo/assistant/test.py
+++ pydemo/assistant/test.py
@@ -2,18 +2,18 @@
 
 
 def test_prompts_classifier():
-    from langchain.llms import OpenAI
+    # from langchain.llms import OpenAI
+    from langchain_community.llms import OpenAI
     # from langchain.chat_models import ChatOpenAI
     # from langchain_community.chat_models import ChatOpenAI    
     from langchain_openai import ChatOpenAI
 
     from prompts.classifier import Classifier
-    from langchain.vectorstores import Chroma
-    from langchain.embeddings.openai import OpenAIEmbeddings
+    # from langchain.vectorstores import Chroma
+    from langchain_community.vectorstores import Chroma
 
-
-    from langchain.vectorstores import Chroma
-    from langchain.embeddings.openai import OpenAIEmbeddings
+    #from langchain.embeddings.openai import OpenAIEmbeddings
+    from langchain_openai import OpenAIEmbeddings
 
     # llm = OpenAI(model_name='text-davinci-003', temperature=0.0)
     llm = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.0)
@@ -29,8 +29,8 @@ def test_prompts_classifier():
     question = 'Create a presentation about climate change.'
     ret = classifier(question)
     print(ret)
-    assert(ret.find('RequiresContext: true') >= 0)
-    assert(ret.find('Categories: presentation') >= 0)
+    assert(ret.content.find('RequiresContext: true') >= 0)
+    assert(ret.content.find('Categories: presentation') >= 0)
 
 def test_prompts_explain():
     from prompts.explain import Explain
@@ -47,5 +47,5 @@ def test_prompts_explain():
 
 
 if __name__ == '__main__':
-    # test_prompts_classifier()
-    test_prompts_explain()
+    test_prompts_classifier()
+    # test_prompts_explain()
