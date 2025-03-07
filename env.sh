8b3d1ca0d79f1d90f694f2a00d2696de5aa5dde8
ling.yuan <cttmayi@hotmail.com>
2025-03-08 00:54:58 +0800

    update env.sh

---
+++ env.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# 获取当前脚本的绝对路径
+current_script=$(realpath "$0")
+
+# 获取当前脚本所在的目录
+current_dir=$(dirname "$current_script")
+
+pylib_dir=$current_dir/pylib
+
+
+# HF 镜像
+export HF_ENDPOINT=https://hf-mirror.com
+
+export PYTHONPATH=$pylib_dir:$PYTHONPATH
+
+# 加载.env文件
+while IFS='=' read -r key value || [[ -n "$key" ]]; do
+    # 跳过空行和注释行
+    if [[ -z "$key" || "$key" =~ ^# ]]; then
+        continue
+    fi
+
+    # 去除键和值两边的空格
+    key=$(echo "$key" | xargs)
+    value=$(echo "$value" | xargs)
+
+    # 导入环境变量
+    # echo "export $key=$value"
+    export "$key=$value"
+done < .env
